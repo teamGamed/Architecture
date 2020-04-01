b443d3ed44a5bcb7c5ce356d00364f01cf9f8364
mahmoodsherif <ma13121999@gmail.com>
2020-04-01 17:51:25 +0200

    add RegisterFile module

---
+++ RegisterFile.vhd
@@ -0,0 +1,23 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity RegisterFile is
+    PORT(
+         read_sel1 : IN  std_logic_vector(4 downto 0);
+         read_sel2 : IN  std_logic_vector(4 downto 0);
+         write_sel : IN  std_logic_vector(4 downto 0);
+         write_ena : IN  std_logic;
+         clk : IN  std_logic;
+         write_data : IN  std_logic_vector(31 downto 0);
+         data1 : OUT  std_logic_vector(31 downto 0);
+         data2 : OUT  std_logic_vector(31 downto 0)
+        );
+end RegisterFile;
+
+architecture Behavioral of RegisterFile is
+
+begin
+
+
+end Behavioral;
+
