c7567269d0f02cc077bff92cc6d2f41470302f03
mahmoodsherif <ma13121999@gmail.com>
2020-04-02 20:44:36 +0200

    fix MUX

--- MUX.vhd
+++ MUX.vhd
@@ -47,7 +47,7 @@ begin
 				r7, r8, r9, r10, r11, r12,
 				r13, r14, r15, r16, r17,
 				r18, r19, r20, r21, r22,
-				r23, r24, r25, r26, r27
+				r23, r24, r25, r26, r27,
 				r28, r29, r30, r31, r32) begin
 	case sel is
 		when "00000" => b <= r1;
